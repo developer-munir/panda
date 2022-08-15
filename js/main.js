8a300e89a9d7ca69085afc67816c135d5dc65f80
developer-munir <devolopermunir@gmail.com>
2022-08-15 13:59:26 +0600

    more js events

--- js/main.js
+++ js/main.js
@@ -12,21 +12,29 @@ navBar.style.backgroundColor = "#007474";
 const buttons = document.getElementsByClassName("btn");
 for (const button of buttons) {
   button.style.backgroundColor = "#007474";
-    button.addEventListener("click", function (event) {
-    event.stopImmediatePropagation();
-    console.log("buy now button is clicked");
-  });
 }
+
+// click handler event
+const caroselBtns=document.getElementsByClassName("carosel-btn");
+for (const caroselBtn of caroselBtns) {
+   caroselBtn.addEventListener("click", function () {
+     console.log("carosel button is clicked");
+   });
+ }
 
 //categories-items
 const categoriesItems = document.getElementsByClassName("categories-items");
 for (const categoriesItem of categoriesItems) {
   categoriesItem.classList.add("categories");
+  // categoriesItem.style.backgroundColor='#007474';
 }
+
 // cards
 const cards = document.getElementsByClassName("card");
 for (const card of cards) {
-  card.classList.add("card-radiaus");
+  card.addEventListener('dblclick', function () {
+    card.classList.add("card-radiaus");
+  })
 }
 
 // email submit button
@@ -41,8 +49,42 @@ emailInput.addEventListener("keyup", function (event) {
     emailBtn.setAttribute("disabled", true);
   }
 });
-emailBtn.addEventListener('click', function (event) {
-    event.stopImmediatePropagation();
-    console.log('email clicked');
+emailBtn.addEventListener("click", function (event) {
+  event.stopImmediatePropagation();
+  console.log(alert('Successfully Send'));
+});
 
+// stay-section dubble click
+document.getElementById("stay-section").addEventListener('dblclick', function () {
+  const staySection = document.getElementById("stay-section");
+  const emailsubBtn = document.getElementById("emailsub-btn");
+  staySection.style.backgroundColor = '#007474';
+  emailsubBtn.style.backgroundColor = '#1c2841';
 })
+
+//buy-nowBtn
+const buyNowBtns=document.getElementsByClassName('buy-nowBtn');
+for (const buyNowBtn of buyNowBtns) {
+  buyNowBtn.addEventListener('click', function (event) {
+    const removeItems = event.target.parentNode.parentNode;
+    const removeItem = removeItems.style.display = "none";
+    console.log(removeItem);
+  })
+}
+
+
+// mouse enter event
+const imgs = document.getElementsByClassName('img-event');
+for (const img of imgs) {
+  img.addEventListener('mouseenter', function () {
+    img.setAttribute('src', 'images/js.jpg');
+  })
+  img.addEventListener('mouseout', function () {
+    imgs[0].setAttribute('src','images/bags/bag-1.png');
+    imgs[1].setAttribute('src','images/bags/bag-2.png');
+    imgs[2].setAttribute('src','images/bags/bag-3.png');
+    imgs[3].setAttribute('src','images/shoes/shoe-1.png');
+    imgs[4].setAttribute('src','images/shoes/shoe-2.png');
+    imgs[5].setAttribute('src','images/shoes/shoe-3.png');
+  })
+}
